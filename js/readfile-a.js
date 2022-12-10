11a880819bb4c1544ddf56339d1bcda8f61ee0df
Subaru <subaru081012@gmail.com>
2022-12-10 17:08:53 +0800

    add promise

--- js/readfile-a.js
+++ js/readfile-a.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
+const fs = require('fs');
 
 // Promise 是一個表示非同步運算的最終完成或失敗的物件。
 let p = new Promise((resolve, reject) => {
   // error-first callback
-  fs.readFile("test.txt", "utf-8", (err, data) => {
+  fs.readFile('test.txt', 'utf-8', (err, data) => {
     if (err) {
       // 如果 err 有值，表示有錯誤發生
       // 這裡應該要處理錯誤
@@ -17,9 +17,20 @@ let p = new Promise((resolve, reject) => {
   });
 });
 
-// 是真正用的人
-p.then((data) => {
-  console.log("我是 then", data);
-}).catch((err) => {
-  console.error("我是 catch", err);
-});
+// async function doJob() {
+//   let data = await p;
+// }
+// // 函式名稱() -> 呼叫
+// doJob();
+
+// IIEF
+(async () => {
+  try {
+    let data = await p;
+    console.log('用 await 拿到的結果', data);
+  } catch (e) {
+    console.error('catch 到的錯誤', e);
+  }
+})();
+
+// (function test() {})();
